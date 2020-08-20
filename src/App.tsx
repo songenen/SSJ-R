1cce3d43f1cc2afff2f2b3fc419677c8717a4361
Songsong <614750803@qq.com>
2020-08-20 15:57:27 +0800

    添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
 height: 100vh;
@@ -17,17 +18,7 @@ const Main = styled.div`
 flex-grow: 1;
 overflow: auto;
 `;
-const Nav = styled.nav`
-border: 1px solid green;
->ul{
-display: flex;
->li{
-width: 33.3333%;
-text-align: center;
-padding: 16px;
-}
-}
-`;
+
 
 function App() {
   return (
@@ -50,19 +41,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签</Link>
-            </li>
-            <li>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );

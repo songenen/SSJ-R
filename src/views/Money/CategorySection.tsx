1e9999837c2bba0785588cd3cc0799600c768374
Songsong <614750803@qq.com>
2020-08-22 13:42:19 +0800

    完成标签的选者和新增

---
+++ src/views/Money/CategorySection.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const CategorySection = styled.section`
+font-size: 24px;
+
+> ul{
+display: flex;
+background: #c4c4c4;
+height: 72px;
+> li{
+width: 50%;
+text-align: center;
+padding: 16px 0;
+position: relative;
+&.selected::after{
+content: '';
+display: block;
+height: 3px;
+background: #333;
+position: absolute;
+bottom: 0;
+width: 100%;
+left: 0;
+}
+}
+}
+`
+export {CategorySection}

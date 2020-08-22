61b7b124ee3d8d22c199b31285b8cb205ce23050
Songsong <614750803@qq.com>
2020-08-22 15:23:55 +0800

    numberpad 功能

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
 font-size: 24px;
 
 > ul{
@@ -24,5 +25,24 @@ left: 0;
 }
 }
 }
-`
-export {CategorySection}
+`;
+
+
+const CategorySection: React.FC = () => {
+  const [categoryList] = useState<('-' | '+')[]>(['-', '+']);
+  const [category, setCategory] = useState('-');
+  const categoryMap = {'-': '支出', '+': '收入'};
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li key={c}
+            className={category === c ? 'selected' : ''}
+              onClick={() => {setCategory(c);}}>
+            {categoryMap[c]}
+          </li>)}
+      </ul>
+    </Wrapper>
+  );
+};
+export {CategorySection};

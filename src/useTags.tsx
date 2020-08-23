d18be86626ab1ef3815c4990328346d8c17f4059
Songsong <614750803@qq.com>
2020-08-23 15:28:39 +0800

    封装findTag到useTags里面

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,11 +1,13 @@
 import {useState} from 'react';
 import {createId} from 'lib/createId';
 
+const defaultTags = [
+  {id: createId(), name: '打车'},
+  {id: createId(), name: '吃饭'},
+];
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    {id: createId(), name: '打车'},
-    {id: createId(), name: '吃饭'},
-  ]);
-  return {tags, setTags};
+  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+  const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+  return {tags, setTags,findTag};
 };
 export {useTags};

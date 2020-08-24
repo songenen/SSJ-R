761b413c62ab2f4b281ea7d834cb972a3ec0d68d
Songsong <614750803@qq.com>
2020-08-24 12:44:07 +0800

    Input.tsx和Button组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+font-size: 16px;
+border: none;
+padding: 8px 12px;
+background: #767676;
+border-radius: 4px;
+color: #fff;
+`;
+export {Button}

97d614c483e553ffd9385947f24dc86f29a2d28c
Songsong <614750803@qq.com>
2020-08-27 16:21:44 +0800

    展示分组记录

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,15 +1,15 @@
 import Layout from '../components/Layout';
-import React, {useState} from 'react';
+import React, {ReactNode, useState} from 'react';
 import {CategorySection} from './Money/CategorySection';
 import styled from 'styled-components';
-import {useRecords} from '../hooks/useRecords';
+import {RecordItem, useRecords} from '../hooks/useRecords';
 import {useTags} from '../hooks/useTags';
-import day from 'dayjs'
+import day from 'dayjs';
 
 const CategoryWrapper = styled.div`
 background: #fff;
 `;
-const Item =styled.div`
+const Item = styled.div`
   display: flex;
   justify-content: space-between;
   background: #fff;
@@ -22,23 +22,53 @@ const Item =styled.div`
   margin-left:  12px;
   font-size: 14px;
   }
+`;
+const Header = styled.h3`
+ padding: 10px 16px;
+ line-height: 20px;
+ font-size: 16px;
 `
 
 function Statistics() {
   const [category, setCategory] = useState<'-' | '+'>('-');
   const {records} = useRecords();
   const {getName} = useTags();
+  const hash:{[K:string]:RecordItem[]}={}
+  const selectedRecords = records.filter(r => r.category === category);
+
+  selectedRecords.map(r=>{
+    const key =day(r.createdAt).format('YYYY-MM-DD')
+      if (!(key in hash)){
+        hash[key]=[]
+      }
+      hash[key].push(r)
+  })
+
+  const array=Object.entries(hash).sort((a,b)=>{
+    if (a[0]===b[0])return 0;
+    if (a[0]>b[0])return -1;
+    if (a[0]<b[0])return 1;
+    return 0;
+  })
+
   return (
     <Layout>
       <CategoryWrapper>
         <CategorySection value={category}
                          onChange={value => setCategory(value)}/>
       </CategoryWrapper>
+      {array.map(([date,records])=><div>
+        <Header>
+          {date}
+        </Header>
       <div>
         {records.map(r => {
           return <Item>
-            <div className="tags">
-              {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+            <div className="tags oneLine" >
+              {r.tagIds.map(tagId => <span key={tagId}>{getName(tagId)}</span>)
+                .reduce((result,span,index,array)=>
+                result.concat(index<array.length-1?[span,',']:[span]),[]as ReactNode[])
+              }
             </div>
             {r.note && <div className="note">
               {r.note}
@@ -46,10 +76,11 @@ function Statistics() {
             <div className="amount">
               ￥{r.amount}
             </div>
-            {/*{day(r.createdAt).format('YYYY年MM月DD日')}*/}
           </Item>;
         })}
       </div>
+        </div>
+      )}
     </Layout>
   );
 }
